2570366a49b4344c11fe692742a1a239d7c52df9
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-27 10:49:26 +0200

    translation files read from resources to stream

--- src/main/java/edu/tseidler/states/Running.java
+++ src/main/java/edu/tseidler/states/Running.java
@@ -1,5 +1,9 @@
 package edu.tseidler.states;
 
+import edu.tseidler.model.Board;
+import edu.tseidler.model.BoardField;
+import edu.tseidler.model.Coordinates;
+
 class Running extends GameState {
 
     public Running(GameState previousState) {
@@ -8,6 +12,11 @@ class Running extends GameState {
 
     @Override
     GameState getNextState() {
+        board = new Board(new int[] {5, 15, 3});
+        board.put(new Coordinates(2, 1), BoardField.X);
+        board.put(new Coordinates(2, 5), BoardField.O);
+        System.out.println((board.put(new Coordinates(6, 5), BoardField.O)));
+        output.accept(board.draw());
         return new GameOverState(this);
     }
 }
