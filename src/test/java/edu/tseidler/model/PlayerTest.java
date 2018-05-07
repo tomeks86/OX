97bdfdbca741d14c98c9e1e121b6e7e6b01e5863
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-07 14:22:17 +0200

    default and irrelevant names extracted to constants

--- src/test/java/edu/tseidler/model/PlayerTest.java
+++ src/test/java/edu/tseidler/model/PlayerTest.java
@@ -1,28 +1,27 @@
 package edu.tseidler.model;
 
-import edu.tseidler.model.BoardField;
-import edu.tseidler.model.Player;
-import edu.tseidler.model.PlayerList;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 @Test
 public class PlayerTest {
+    public static final String PLAYER1_NAME = "stefan";
+    public static final String PLAYER2_NAME = "franek";
     private Player player1;
     private Player player2;
     private PlayerList playerList;
 
     @BeforeMethod
     public void setUpPlayers() {
-        player1 = new Player("stefan", BoardField.O, true);
-        player2 = Player.second(player1, "franek");
+        player1 = new Player(PLAYER1_NAME, BoardField.O, true);
+        player2 = Player.second(player1, PLAYER2_NAME);
     }
 
     public void shouldCreatePlayers() {
         SoftAssert sa = new SoftAssert();
-        sa.assertEquals(player1.getName(), "stefan");
-        sa.assertEquals(player2.getName(), "franek");
+        sa.assertEquals(player1.getName(), PLAYER1_NAME);
+        sa.assertEquals(player2.getName(), PLAYER2_NAME);
         sa.assertEquals(player1.getMark(), BoardField.O);
         sa.assertEquals(player2.getMark(), BoardField.X);
         sa.assertTrue(player1.isFirst());
