c2c47db5f94bba8f136d05bf523b4dadcb4f4cda
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-11 16:24:54 +0200

    enhanced support for input implemented

--- src/main/java/edu/tseidler/Main.java
+++ src/main/java/edu/tseidler/Main.java
@@ -3,24 +3,64 @@ package edu.tseidler;
 import edu.tseidler.service.CommandLineSupplier;
 import edu.tseidler.service.FileLineSupplier;
 import edu.tseidler.states.OXGame;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Main {
+
+    private static final Supplier<String> DEFAULT_STRING_SUPPLIER = new Scanner(System.in)::nextLine;
+    private static final Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
 
-        Supplier<String> inputSupplier = new Scanner(System.in)::nextLine;
-        if (args.length > 0 && args[0].equalsIgnoreCase("-c")) {
-            inputSupplier = new CommandLineSupplier(Arrays.stream(args).skip(1));
-        } else if (args.length > 1 && args[0].equalsIgnoreCase("-f")) {
-            inputSupplier = new FileLineSupplier(args[1]);
-        }
+        final Properties defaultProperties = new Properties();
+        defaultProperties.setProperty("input", "stdin");
+        defaultProperties.setProperty("output", "stdout");
+        defaultProperties.setProperty("language", "en");
+        defaultProperties.setProperty("player1_name", "Jacek");
+        defaultProperties.setProperty("player1_mark", "X");
+        defaultProperties.setProperty("player1_first", "true");
+        defaultProperties.setProperty("player2_name", "Placek");
+        defaultProperties.setProperty("player2_mark", "O");
+        defaultProperties.setProperty("player2_first", "false");
+        defaultProperties.setProperty("board_rows", "3");
+        defaultProperties.setProperty("board_cols", "3");
+        defaultProperties.setProperty("board_winn", "3");
+
+
+        Properties properties = readProperties();
+
+        Map<String, Supplier<String>> supplierMap = new HashMap<String, Supplier<String>>() {{
+            put("stdin", DEFAULT_STRING_SUPPLIER);
+            try {
+                put("file", new FileLineSupplier(Main.class.getResource((String) properties.getOrDefault("input_file", "scenarios/error")).getPath()));
+            } catch (NullPointerException e) {
+                logger.log(Level.WARN, "input file in settings.properties doesn't exist - falling back to console");
+                put("file", DEFAULT_STRING_SUPPLIER);
+            }
+        }};
+        Supplier<String> inputSupplier = supplierMap.getOrDefault(properties.get("input"), DEFAULT_STRING_SUPPLIER);
 
         Consumer<String> output = System.out::println;
 
         new OXGame(inputSupplier, output, "en").start();
+    }
+
+    private static Properties readProperties() {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = Main.class.getResourceAsStream("settings.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
     }
 }
--- src/main/java/edu/tseidler/service/FileLineSupplier.java
+++ src/main/java/edu/tseidler/service/FileLineSupplier.java
@@ -1,16 +1,24 @@
 package edu.tseidler.service;
 
+import edu.tseidler.Main;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileLineSupplier implements Supplier<String> {
     List<String> lines = new LinkedList<>();
+    Scanner scanner;
+    boolean finished = false;
+    private static final Logger logger = Logger.getLogger(Main.class);
 
     public FileLineSupplier(String path) {
         try (Stream<String> file_lines = Files.lines(Paths.get(path))) {
@@ -24,6 +32,14 @@ public class FileLineSupplier implements Supplier<String> {
     public String get() {
         if (!lines.isEmpty())
             return lines.remove(0);
-        else return "";
+        else {
+            if (!finished) {
+                logger.log(Level.INFO, "file finished switching to console input");
+                scanner = new Scanner(System.in);
+                finished = true;
+            }
+            lines.add(scanner.nextLine());
+            return lines.remove(0);
+        }
     }
 }
