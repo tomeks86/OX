3283a3b6376281f851fa18833c208d648df539af
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-14 18:33:55 +0200

    fat jar corrected

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 PROJECT=OX
 VERSION=1.2.0-SNAPSHOT
 
-if [ ! -e target/${PROJECT}-${VERSION}.jar ]; then
+if [ ! -e target/${PROJECT}-${VERSION}-jar-with-dependencies.jar ]; then
 	mvn clean install
 fi
 
-java -jar target/${PROJECT}-${VERSION}.jar src/main/resources/edu/tseidler/settings.properties
+java -jar target/${PROJECT}-${VERSION}-jar-with-dependencies.jar src/main/resources/edu/tseidler/settings.properties
